e6566833d7d3986672eb97966397c8b645e40a06
ytliveloose-owit <yuto.rk0829@gmail.com>
2026-07-20 15:37:34 +0900

    Update stock_scan.py

--- stock_scan.py
+++ stock_scan.py
@@ -1,1 +1,9 @@
-print("GitHub Actionsから実行成功！")
+import os
+
+api_key = os.getenv("JQUANTS_API_KEY")
+
+if api_key:
+    print("APIキーを正常に読み込みました。")
+    print(f"キーの先頭5文字: {api_key[:5]}*****")
+else:
+    print("APIキーが読み込めません。")

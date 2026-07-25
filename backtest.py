4269a0902305ba1578850097ffd58cf69331cf23
ytliveloose-owit <yuto.rk0829@gmail.com>
2026-07-25 16:44:22 +0900

    Update backtest.py

--- backtest.py
+++ backtest.py
@@ -1,1 +1,47 @@
+from datetime import datetime
+import pandas as pd
+import jquantsapi
 
+# ==========================
+# J-Quants
+# ==========================
+cli = jquantsapi.ClientV2()
+
+# ==========================
+# 取得期間
+# ==========================
+start_dt = datetime(2023, 1, 1)
+end_dt = datetime(2025, 12, 31)
+
+# ==========================
+# 日足取得
+# ==========================
+df = cli.get_eq_bars_daily_range(
+    start_dt=start_dt,
+    end_dt=end_dt
+)
+
+# ==========================
+# 並び替え
+# ==========================
+df = df.sort_values(
+    ["Code", "Date"]
+)
+
+print(df.head())
+print(df.tail())
+
+print()
+
+print("取得件数")
+print(len(df))
+
+print()
+
+print("銘柄数")
+print(df["Code"].nunique())
+
+print()
+
+print("営業日数")
+print(df["Date"].nunique())

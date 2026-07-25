b111eefd76b49e09439e07f73a0bc9e03bef6fc5
ytliveloose-owit <yuto.rk0829@gmail.com>
2026-07-25 21:26:37 +0900

    Update backtest.py

--- backtest.py
+++ backtest.py
@@ -120,13 +120,13 @@ df["NextClose"] = (
 # ==========================
 
 signal = df[
-    (df["ChangeRate"] >= -3) &
-    (df["ChangeRate"] <= -1) &
+    (df["ChangeRate"] >= -5) &
+    (df["ChangeRate"] <= -2) &
     (df["AdjC"] >= 500) &
     (df["AdjC"] <= 5000) &
     (df["AdjVo"] >= 100000) &
-    (df["TradingValue"] >= 100000000) &
-    (df["AdjVo"] >= df["AvgVol5"]*1.2) &      # 出来高20％増
+    (df["TradingValue"] >= 300000000) &
+    (df["AdjVo"] >= df["AvgVol5"]*1.5) &      # 出来高50％増
     (df["AdjC"] <= df["BB_Lower"] * 1.00) &   # BB下限割れ
     (df["RSI14"] <= 25)                       # RSI25以下
 ]

f14fd654a984aa01e2d216d7321413710a287158
ytliveloose-owit <yuto.rk0829@gmail.com>
2026-08-02 10:13:44 +0900

    backtest.py を更新

--- backtest.py
+++ backtest.py
@@ -234,7 +234,7 @@ signal = df[
     (df["AdjC"] <= df["BB_Lower"] * 1.01) &
 
     # RSI33.5以下
-    (df["RSI14"] <= 33.5) &
+    (df["RSI14"] <= 35) &
 
     # 当日陽線
     ((df["AdjC"] - df["AdjO"]) / df["AdjO"] >= 0.01) &
